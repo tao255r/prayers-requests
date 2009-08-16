0d9d345ec071acd5b06c01df1268210bfd2bb398
hypocretan <hypocretan@74942b2a-34f1-11de-9e80-b52b74bdfc52>
2009-08-16 13:21:33 +0000

    Static Resources and Static Files (Broom closet theme)

--- trunk/common/jquery/settings.py
+++ trunk/common/jquery/settings.py
@@ -6,4 +6,8 @@ settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
     'jquery/jquery.bgiframe.js',
     'jquery/jquery.livequery.js',
     'jquery/jquery.form.js',
-)
+    'jquery/jquery.validate.js',
+    'jquery/jquery.datePicker.js',
+    'jquery/date.js',
+    'jquery/tabber-minimized.js',
+	}
